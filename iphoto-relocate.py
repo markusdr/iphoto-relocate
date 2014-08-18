735959230919f6a39bde3609b9f181cecb37417e
Markus Dreyer <markus.dreyer@gmail.com>
2014-08-17 21:18:40 -0700

    added more file types, used faster file copy code

--- iphoto-relocate.py
+++ iphoto-relocate.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Markus Dreyer, mdreyer@sdl.com
+# Markus Dreyer, markus.dreyer@gmail.com
 
 from __future__ import print_function
 import optparse
@@ -15,14 +15,6 @@ import shutil
 
 LOG = logging.getLogger(__name__)
 
-def mkdir_p(path):
-    try:
-        os.makedirs(path)
-    except OSError as exc:
-        if exc.errno == errno.EEXIST and os.path.isdir(path):
-            pass
-        else: raise
-
 # from http://blogs.blumetech.com/blumetechs-tech-blog/2011/05/faster-python-file-copy.html
 def copy_file(src, dst, buffer_size=10485760):
     '''
@@ -32,12 +24,11 @@ def copy_file(src, dst, buffer_size=10485760):
     @param buffer_size:    Buffer size to use during copy
     @param perserveFileDate:    Preserve the original file date
     '''
-    #    Check to make sure destination directory exists. If it doesn't create the directory
     dstParent, dstFileName = os.path.split(dst)
     if not os.path.exists(dstParent):
         os.makedirs(dstParent)
     
-    #    Optimize the buffer for small files
+    # Optimize the buffer for small files
     buffer_size = min(buffer_size, os.path.getsize(src))
     if(buffer_size == 0):
         buffer_size = 1024
@@ -46,10 +37,9 @@ def copy_file(src, dst, buffer_size=10485760):
         try:
             st = os.stat(fn)
         except OSError:
-            # File most likely does not exist
-            pass
+            pass  # File most likely does not exist
         else:
-            # XXX What about other special files? (sockets, devices...)
+            # TODO: What about other special files? (sockets, devices...)
             if shutil.stat.S_ISFIFO(st.st_mode):
                 raise shutil.SpecialFileError("`%s` is a named pipe" % fn)
 
@@ -58,10 +48,30 @@ def copy_file(src, dst, buffer_size=10485760):
     shutil.copyfileobj(fsrc, fdst, buffer_size)
     shutil.copystat(src, dst)
 
-def main(argv):
-    """Relocate iPhoto Masters files"""
+def is_media_file(fname):
+    """True for filenames with image or video extensions"""
+    f = fname.lower()
+    return f.endswith('.jpg') or f.endswith('.gif') or f.endswith('.tiff') or f.endswith('.png') or f.endswith('.mpg') or f.endswith('.mov') or f.endswith('.divx') or f.endswith('.mp4') or f.endswith('.wmv')
 
-    parser = optparse.OptionParser("usage: %prog [options] args",
+def maybe_copy(src, dst, do_copy, dry_run):
+    """Copy file over unless options don't allow it. Returns true if copied (even in dry run)"""
+    if os.path.isfile(dst): # Okay, file already exists
+        if not os.path.getsize(src) == os.path.getsize(dst):
+            raise Exception('File sizes differ: {0} vs {1}'.format(src, dst) )
+        return False
+    if os.path.isdir(dst):
+        raise Exception('Already exists as directory: ' + dst)
+    if not do_copy:
+        raise Exception('File does not exist in target location, but option --nocopy prevents us from copying it over')
+    LOG.debug('Copying ' + src + " to " + dst)
+    if not dry_run:
+        copy_file(src, dst)
+    return True
+
+def main(argv):
+    """Relocate iPhoto Masters files to free up space."""
+
+    parser = optparse.OptionParser("Usage: %prog [options] <iphoto-library-path> <target-path>",
                                    version="%prog 1.0")
 
     parser.add_option("--debug",
@@ -69,6 +79,12 @@ def main(argv):
                       action="store_true",
                       default=False,
                       help="enable debug logging")
+
+    parser.add_option("--nocopy",
+                      dest="nocopy",
+                      action="store_true",
+                      default=False,
+                      help="Use this option if all files are already in the target directory (e.g., after copying iPhoto library over by hand, using rsync, etc.)")
 
     parser.add_option("--dry-run",
                       dest="dry_run",
@@ -86,36 +102,28 @@ def main(argv):
         LOG.debug("Enabled debug logging")
         LOG.debug("Options:\n" + str(opts))
 
-    dry_run = True if opts.dry_run else False
+    dry_run = opts.dry_run
+    do_copy = not opts.nocopy
 
     old = os.path.abspath(args[0] + "/Masters")
-    new = os.path.abspath(args[1])
+    new = os.path.abspath(args[1] + "/Masters")
 
     if not os.path.isdir(old):
         raise Exception('Could not find ' + old)
-    if not os.path.isdir(old):
-        raise Exception('Could not find ' + old)
+    if not os.path.isdir(new):
+        os.makedirs(new)
 
     num_copied = 0
     num_linked = 0
     os.chdir(old)
     for dirpath, dirnames, filenames in os.walk('.'):
         for f in filenames:
-            f_low = f.lower()
-            if f_low.endswith('.jpg') or f_low.endswith('.mpg') or f_low.endswith('.mov'):
+            if is_media_file(f):
                 old_file = dirpath + '/' + re.sub('^\./', '', f)
-                if not os.path.islink(old_file):
+                if not os.path.islink(old_file): # could already be a link, e.g. due to previous run of this script
                     new_file = os.path.realpath(new + '/' + old_file)
-                    if not os.path.isfile(new_file):
-                        new_dir = new + '/' + os.path.dirname(old_file)
-                        LOG.debug('mkdir ' + new_dir)
-                        LOG.debug('copy ' + old_file + " to " + new_file)
-                        if not dry_run:
-                            mkdir_p(new_dir)
-                            shutil.copyfile(old_file, new_file)
-                            shutil.copystat(old_file, new_file)
+                    if maybe_copy(old_file, new_file, do_copy, dry_run):
                         num_copied += 1
-                    LOG.debug(old_file + ' => ' + new_file)
                     if not dry_run:
                         os.remove(old_file)
                         os.symlink(new_file, old_file)
